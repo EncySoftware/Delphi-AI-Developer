611e72d94b16cc261c00682eec120057db3094b3
duromanov <duromanov@encycam.io>
2026-01-22 17:20:56 +0300

    fix ConfReturnAI func for code completion

--- Src/Utils/DelphiAIDev.Utils.pas
+++ Src/Utils/DelphiAIDev.Utils.pas
@@ -377,37 +377,10 @@ begin
 end;
 
 class function TUtils.ConfReturnAI(const AValue: string): string;
-var
-  LStrings: TStrings;
-  LStrLine: string;
-  i: Integer;
 begin
-  Result := AValue.Trim
-    .Replace(TConsts.MARK_BEGIN_DELPHI, '', [rfReplaceAll, rfIgnoreCase])
-    .Replace(TConsts.MARK_BEGIN_PASCAL, '', [rfReplaceAll, rfIgnoreCase])
-    //.Replace(TConsts.MARK_BEGIN_PASCAL2, '', [rfReplaceAll, rfIgnoreCase])
-    .Replace(TConsts.MARK_BEGIN_SQL, '', [rfReplaceAll, rfIgnoreCase])
-    .Replace(TConsts.MARK_BEGIN_SQL2, '', [rfReplaceAll, rfIgnoreCase])
-    .Replace(TConsts.MARK_END, '', [rfReplaceAll, rfIgnoreCase]);
-
-  LStrings := TStringList.Create;
-  try
-    LStrings.Text := Result;
-
-    Result := '';
-    for i := 0 to Pred(LStrings.Count) do
-    begin
-      LStrLine := LStrings[i];
-      if (i = 0) and ((LStrLine.Trim.IsEmpty)or(LStrLine = sLineBreak)) then
-        Continue;
-
-      Result := Result + LStrLine + sLineBreak;
-    end;
-
-    Result := Result.TrimRight;
-  finally
-    LStrings.Free;
-  end;
+  Result := AValue
+    .Replace('\n', sLineBreak, [rfReplaceAll, rfIgnoreCase])
+    .TrimRight;
 end;
 
 class function TUtils.ProcessTextForEditor(const AText: string): string;
